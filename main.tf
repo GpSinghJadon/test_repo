f5790e359d6392a718d6018d4057cd66467ca0dd
gajensin <gajensin@cisco.com>
2020-10-12 23:57:08 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,41 +1,23 @@
-provider "azurerm" {
-  
-  version = "=2.20.0"
-  subscription_id = "6d5ae732-b526-4f92-a94b-e7f460d3733a"
-  client_id       = "935fb058-e6e1-4d48-9e21-47380b15b951"
-  client_secret   = "3B51T_5j~40YpOd.rIrZg6~sdzX~dP.8VA"
-  tenant_id       = "b066b0b3-57a6-451f-9111-12a529a39311"
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "azure-functions-test-rg"
-  location = "westus2"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "functionsapptestsa123456"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-test-service-plan1234"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_function_app" "example" {
-  name                      = "test-azure-functions-for-pradeep-demo"
-  location                  = azurerm_resource_group.example.location
-  resource_group_name       = azurerm_resource_group.example.name
-  app_service_plan_id       = azurerm_app_service_plan.example.id
-  storage_connection_string = azurerm_storage_account.example.primary_connection_string
-}
+ resource "azurerm_storage_account" "storage_account" {
+  name = var.storage_account_name
+  resource_group_name = var.resource_group_name
+  location = var.location
+  account_tier = var.account_tier
+  account_replication_type = var.account_replication_type
+ }
+ resource "azurerm_app_service_plan" "app_service_plan" {
+  name = var.app_service_plan_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  sku = {
+    tier = var.sku_tier
+    size = var.sku_size
+   }
+ }
+ resource "azurerm_function_app" "function_app" {
+  name = var.function_app_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = var.app_service_plan_id
+  storage_connection_string = var.storage_connection_string
+ }
