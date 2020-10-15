1f613688aa6a35d845e2819fecb819067250c8a2
gajensin <gajensin@cisco.com>
2020-10-15 22:10:46 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,35 +1,23 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "azure-functions-test-rg"
-  location = "westus2"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "functionsapptestsa123456"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = var.account_tier
+ resource "azurerm_storage_account" "storage_account" {
+  name = var.storage_account_name
+  resource_group_name = var.resource_group_name
+  location = var.location
+  account_tier = var.account_tier
   account_replication_type = var.account_replication_type
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-test-service-plan1234"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = 55
-  }
-}
-
-resource "azurerm_function_app" "example" {
-  name                      = "test-azure-functions-for-pradeep-demo"
-  location                  = azurerm_resource_group.example.location
-  resource_group_name       = azurerm_resource_group.example.name
-  app_service_plan_id       = azurerm_app_service_plan.example.id
-  storage_connection_string = azurerm_storage_account.example.primary_connection_string
-}
+ }
+ resource "azurerm_app_service_plan" "app_service_plan" {
+  name = var.app_service_plan_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  sku = {
+    tier = var.sku_tier
+    size = var.sku_size
+   }
+ }
+ resource "azurerm_function_app" "function_app" {
+  name = var.function_app_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = var.app_service_plan_id
+  storage_connection_string = var.storage_connection_string
+ }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,44 @@
-variable "account_replication_type" {
+variable "storage_account_name" {
+    type = string
+}
+
+variable "resource_group_name" {
+    type = string
+}
+
+variable "location" {
     type = string
 }
 
 variable "account_tier" {
     type = string
 }
+
+variable "account_replication_type" {
+    type = string
+}
+
+variable "app_service_plan_name" {
+    type = string
+}
+
+variable "sku_tier" {
+    type = string
+}
+
+variable "sku_size" {
+    type = number
+}
+
+variable "function_app_name" {
+    type = string
+}
+
+variable "app_service_plan_id" {
+    type = string
+}
+
+variable "storage_connection_string" {
+    type = string
+}
+
